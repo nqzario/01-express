92ac07efae2840e16b5fc303462f197d897dbfb1
elizabet <evzukr@gmail.com>
2026-01-19 18:12:18 +0100

    first comit

--- src/server.js
+++ src/server.js
@@ -1,51 +1,39 @@
 import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
+import { errorHandler } from './middleware/errorHandler.js';
+import { logger } from './middleware/logger.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { connectMongoDB } from './db/connectMongoDB.js';
+import notesRoutes from './routes/notesRoutes.js';
 import 'dotenv/config';
 
 const PORT = process.env.PORT ?? 3000;
 const app = express();
 
-app.use(cors({
+app.use(
+  cors({
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-}));
+  }),
+);
 app.use(helmet());
-app.use(express.json({
+
+app.use(logger);
+
+app.use(
+  express.json({
     limit: '5mb',
-}));
+  }),
+);
 
-app.get('/notes', (req, res) => {
-    res.status(200).json({
-	"message": "Retrieved all notes"
-    });
-});
+app.use(notesRoutes);
 
-app.get('/test-error', () => {
-  throw new Error('Simulated server error');
-});
+app.use(notFoundHandler);
 
+app.use(errorHandler);
 
-app.get('/notes/:noteId', (req, res) => {
-    res.status(200).json({
-    "message": `Retrieved note with ID: ${req.params.noteId}`
-});
-});
-
-app.use((req, res) => {
-    res.status(404).json({
-        message: 'Route not found',
-    });
-});
-
-app.use((err, req, res, next)=> {
-    console.log(err.message);
-    
-    res.status(500).json({
-        message: "Something went wrong",
-        error: err.message, 
-    });
-});
+await connectMongoDB();
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
